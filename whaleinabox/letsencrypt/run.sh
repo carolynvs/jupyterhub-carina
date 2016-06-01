8d29e4c17d30a3b93c658a32ae6b52c200cbc70d
carolynvs <me@carolynvanslyck.com>
2016-05-31 22:02:14 -0500

    Make letsencrypt image usable without a custom build

--- whaleinabox/letsencrypt/run.sh
+++ whaleinabox/letsencrypt/run.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -euo pipefail
 
+# Install the docker client
+echo "Checking for the Docker client"
+if [ ! -f "/usr/local/bin/docker" ]; then
+  echo "Installing Docker $DOCKER_VERSION..."
+  curl https://get.docker.com/builds/Linux/x86_64/docker-$DOCKER_VERSION.tgz -o /tmp/docker-$DOCKER_VERSION.tgz
+  tar -xzf /tmp/docker-$DOCKER_VERSION.tgz -C /tmp
+  cp /tmp/docker/docker /usr/local/bin/docker
+fi
+
 # Wait for nginx as our Let's Encrypt challenge is served by it
 echo "Waiting for nginx to start..."
 until $(curl --output /dev/null --silent --head --fail --insecure https://$DOMAIN); do
