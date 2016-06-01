6e6e3b09c7f7d9d05eb6a6a74283b648382f6cec
carolynvs <me@carolynvanslyck.com>
2016-05-31 22:38:06 -0500

    Move whaleinabox user customized files into separate folder
    Prepare to move whaleinabox/whaleinabox into a new repo for easy cloning.

--- whaleinabox/whaleinabox/run.sh
+++ whaleinabox/whaleinabox/run.sh
@@ -6,26 +6,26 @@ __SCRIPT_SOURCE="$_"
 if [ -n "$BASH_SOURCE" ]; then
   __SCRIPT_SOURCE="${BASH_SOURCE[0]}"
 fi
-DIR="$(cd "$(dirname "${__SCRIPT_SOURCE:-$0}")" > /dev/null && \pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${__SCRIPT_SOURCE:-$0}")" > /dev/null && \pwd)"
 unset __SCRIPT_SOURCE 2> /dev/null
 
 # Load secret credentials
-source $DIR/secrets.sh
+source $SCRIPT_DIR/secrets.sh
 
 # Load settings
-source $DIR/settings.sh
+source $SCRIPT_DIR/settings.sh
 
 # Connect to the Carina cluster
 eval $(carina env $JUPYTERHUB_CLUSTER)
 
 # Cleanup old containers
-docker rm -f nginx jupyterhub letsencrypt &> /dev/null || true
+docker rm -f nginx jupyterhub letsencrypt website &> /dev/null || true
 
 # Build and publish the custom Docker image used by the user's Jupyter server
-docker build -t $JUPYTER_IMAGE jupyter
+docker build -f $SCRIPT_DIR/Dockerfile-jupyter -t $JUPYTER_IMAGE $SCRIPT_DIR
 docker push $JUPYTER_IMAGE
 
 # Do all the things with a sidecar of stuff
-docker-compose build
-docker-compose up -d
-docker-compose logs -f
+docker-compose -f $SCRIPT_DIR/docker-compose.yml build
+docker-compose -f $SCRIPT_DIR/docker-compose.yml up -d
+docker-compose -f $SCRIPT_DIR/docker-compose.yml logs -f
